9bcaa9536f858f58cb4f2cc54e1d6ab7450f4003
jasonfernandes <50226450+jasonfernandes@users.noreply.github.com>
2021-09-12 22:35:16 +0530

    Update text.php

--- DSATM_Student_Result_Web_Log-main/text.php
+++ DSATM_Student_Result_Web_Log-main/text.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'bharath', '12345', 'student_data_base');
+$conn = mysqli_connect('localhost', 'jason', '12345', 'student_data_base');
 if(!$conn){
 	echo 'Connection error: '. mysqli_connect_error();
 }
